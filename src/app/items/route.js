43f2dda93f72294ddafd30e2c0fee8fcaf870984
GladsIA <project.gladsia@gmail.com>
2025-03-24 18:07:40 -0300

    Test: Uso da api com a openai

--- src/app/items/route.js
+++
@@ -1,5 +0,0 @@
-import { handleCors } from '@/controllers/corsControllers';
-import { handlePostItems } from '@/controllers/itemControllers';
-
-export { handleCors as OPTIONS };
-export { handlePostItems as POST };
