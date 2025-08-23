0ddef87488796be20cafce87c3b434c967c2143f
CamelComp <project.gladsia@gmail.com>
2025-08-22 21:57:21 -0300

    test: autenticação da resposta do hubspot

--- src/app/hubspot/oc/route.js
+++ src/app/hubspot/oc/route.js
@@ -1,6 +1,29 @@
+import crypto from 'crypto';
 import { NextResponse } from 'next/server';
 
-export async function POST(req, res){
+export async function POST(request){
+    const headers = Object.fromEntries(request.headers.entries());
+    const body = await request.text();
+    console.log('HUBSPOT → webhook HIT', { headers, body });
+    const signature = headers['x-hubspot-signature-v3'];
+    const timestamp = headers['x-hubspot-request-timestamp'];
+    console.log('HUBSPOT → signature', signature);
+    console.log('HUBSPOT → timestamp', timestamp);
+    const clientSecret = process.env.AUTOMACAO_OC_APP_HUBSPOT_CLIENT_SECRET;
+    const baseString = `v3/${timestamp}/${body}`;
+    const hmac = crypto
+        .createHmac('sha256', clientSecret)
+        .update(baseString)
+        .digest('base64');
+    console.log('HUBSPOT → hmac', hmac);
+    if(hmac !== signature){
+        return NextResponse.json(
+            { success: false, message: 'Invalid signature' },
+            { status: 401 }
+        );
+    }
+    const parsed = JSON.parse(body);
+    console.log('Webhook válido →', parsed);
     return NextResponse.json(
         { success: true, message: 'Route successfully called' }, 
         { status: 200 }
